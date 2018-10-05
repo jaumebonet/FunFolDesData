feedd0cac17f49375ed0dbaf08ab0a7e63f80c48
Jaume Bonet Martinez <jaume.bonet@gmail.com>
2018-10-05 15:04:59 +0200

    1kx8 relax

---
+++ 03.designs/1kx8/selection/baseline/relax/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --partition=serial
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 4096
+#SBATCH --time 03:00:00
+#SBATCH --array=1-50
+#SBATCH --output=log/abi.%A_%a.out
+#SBATCH --error=log/abi.%A_%a.err
+
+
+NSTRUCT="-nstruct 1"
+
+# JOB
+INPUTS="-in:file:s ../1kx8_trimmed.pdb -in:file:native ../1kx8_trimmed.pdb"
+
+mkdir -p  relkoys/
+srun /work/upcorreia/bin/rosetta_bin/stable/RosettaWeekly/rosetta_scripts.linuxiccrelease -parser:protocol relax.xml ${INPUTS} ${NSTRUCT} -out:file:scorefile relkoys.sc -out:path:pdb relkoys/ -out:suffix ${SLURM_ARRAY_TASK_ID}  -overwrite
